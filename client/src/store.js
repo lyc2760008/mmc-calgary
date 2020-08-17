8f4c2cd4394fd159d68cc812153522650506eac5
lyc2760008 <44538498+lyc2760008@users.noreply.github.com>
2020-08-16 19:02:08 -0600

    script fixed

--- client/src/store.js
+++ client/src/store.js
@@ -15,7 +15,11 @@ const store = createStore(
 
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? a => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
